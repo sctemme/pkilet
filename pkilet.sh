35380ada0b94fdba8606e0d8246fbb9f083b3f79
Sander Temme <sander@temme.net>
2014-04-04 22:08:17 -0700

    Add issuing CA.  Change root key to 4096, hashes to sha256 throughout

--- pkilet.sh
+++ pkilet.sh
@@ -19,8 +19,18 @@ CAKEY=./cakey.pem
 CAREQ=./careq.pem
 CACERT=./cacert.pem
 
-# newca() {
-# }
+mkdirs() {
+    topdir=$1
+    # Blow away the old one, no niceties.
+    if [ -d "$topdir" ] ; then rm -rf $topdir ; fi
+    # create the directory hierarchy
+	mkdir -p ${topdir}
+	mkdir -p ${topdir}/certs
+	mkdir -p ${topdir}/crl
+	mkdir -p ${topdir}/newcerts
+	mkdir -p ${topdir}/private
+	touch ${topdir}/index.txt
+}
 
 RET=0
 
@@ -32,23 +42,29 @@ case $1 in
     ;;
 -newroot)
     caname=rootCA
-    # Blow away the old one, no niceties.
-    if [ -d "$caname" ] ; then rm -rf $caname ; fi
-    # create the directory hierarchy
-	mkdir -p ${caname}
-	mkdir -p ${caname}/certs
-	mkdir -p ${caname}/crl
-	mkdir -p ${caname}/newcerts
-	mkdir -p ${caname}/private
-	touch ${caname}/index.txt
-
-    echo "Making CA certificate ..."
-    $REQ -new -keyout ${caname}/private/$CAKEY -nodes\
-           -out ${caname}/$CAREQ -subj "/C=US/ST=Colorado/O=Apache Software Foundation/CN=PKI-Let Root CA" -verbose
+    mkdirs ${caname}
+    echo "Making Root CA certificate ..."
+    $REQ -new -newkey rsa:4096 -keyout ${caname}/private/$CAKEY -nodes -out ${caname}/$CAREQ \
+           -subj "/C=US/ST=Colorado/O=Apache Software Foundation/CN=PKI-Let Root CA"
     $CA -create_serial -out ${caname}/$CACERT $CADAYS -batch \
            -keyfile ${caname}/private/$CAKEY -selfsign \
            -extensions v3_ca -name ${caname} \
            -infiles ${caname}/$CAREQ 
+    RET=$?
+    ;;
+-newissuing)
+    caname=issuingCA
+    mkdirs ${caname}
+    echo "Making Issuing CA certificate ..."
+    $REQ -new -newkey rsa:2048 -keyout ${caname}/private/$CAKEY -nodes -out ${caname}/$CAREQ \
+         -subj "/C=US/ST=Colorado/O=ASF/OU=Apache HTTP Server/CN=PKI-Let Issuing CA"
+    # Now sign it with the root CA
+    $CA -name rootCA -policy policy_anything -out ${caname}/$CACERT -batch \
+        -extensions v3_ca -infiles ${caname}/$CAREQ
+    # Somehow from CA.sh it seems that the serial # for the new CA comes from its certificate
+    # The -next_serial option used here seems undocumented
+    $X509 -in ${caname}/$CACERT -noout -next_serial \
+          -out ${caname}/serial
     RET=$?
     ;;
 *)
